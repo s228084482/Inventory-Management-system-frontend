51e1ed6256c51f1ad042bce9ad9ead447f6833b5
Philasande <s228084482@mandela.ac.za>
2026-02-02 23:08:01 +0200

    Initial commit: Adding froend project for the first time.

---
+++ pages/Scipts/frontEndScript.js
@@ -0,0 +1,23 @@
+
+
+
+// fetch("http://localhost:8080/api/users")
+// .then(respond => respond.json())
+// .then(data =>{
+
+// }).catch(error => console.error("Error",error));
+
+// // dashboard scripting =================================================
+// // getting all users
+// fetch("http://localhost:8080/api/users/total")
+// .then(response => response.json())
+// .then(total =>{
+//     document.getElementById("systemUsersNo").innerText = total;
+// }).catch(error => console.error(error));
+
+
+// fetch("http://localhost:8080/api/users/active")
+// .then(response => response.json())
+// .then(activeUsers =>{
+//     document.getElementById("userNo").innerText = activeUsers;
+// }).catch(error => console.error(error));
