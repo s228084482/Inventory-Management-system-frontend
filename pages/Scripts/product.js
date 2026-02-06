4abf2a096adf7cb4eeb84c9434271d6ebc76aa25
Philasande <s228084482@mandela.ac.za>
2026-02-06 22:37:24 +0200

    Almost dome with product use case

---
+++ pages/Scripts/product.js
@@ -0,0 +1,73 @@
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn_add = document.getElementById("btn_addProduct");
+
+    if (btn_add) {
+        btn_add.addEventListener("click", () => {
+            window.location.href = "add_product.html";
+        });
+    }
+});
+const productForm = document.getElementById("addProductForm");
+if (productForm) {
+    productForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        const productName = formData.get("productName");
+        const price = formData.get("price");
+        const quantity = formData.get("quantity");
+        const description = formData.get("description");
+
+        console.log(productName, price, quantity, description);
+
+        fetch("http://localhost:8080/api/products/save", {
+            method: "POST",
+            body: formData  //i am sending this data to the backend//
+        }).then(response => response.text)
+            .then(result => {
+                console, log(result);
+                alert("Product saved successfully");
+            }).catch(err => console.error(err));
+    });
+}
+
+
+document.getElementById("formProductInfo").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const dataForm = new dataForm(event.target);
+
+    fetch("http://localhost:8080/api/products/saveProductInfo", {
+        method: "POST",
+        body: dataForm
+    }).then(response => response.text)
+        , then(result => {
+            console.log(result);
+            alert("Saved successfully.");
+        }).catch(err => console.error(err));
+
+});
+
+
+fetch("http://localhost:8080/api/products/getProduct")
+    .then(response => response.json())
+    .then(products => {
+        const tablebody = document.querySelector("#productTable tbody");
+        tablebody.innerHTML = "";
+        
+        // products.forEach(product => {
+        //     const row = document.createElement("tr");
+
+        //     row.innerHTML = '
+        //     <td>${product.}</td>
+        //     <td></td>
+        //     <td></td>
+        //     <td></td>
+        //     ';
+        // });
+    })
